074600ccc67286f0862a1d897bae66051bb2518d
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2022-11-28 15:57:55 +0500

    gov

--- pages/talaba/talaba.js
+++ pages/talaba/talaba.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { items4 } from '../../components/data/items';
+// import { items4 } from '../../components/data/items';
 const Talaba = () => {
     return (
         <div className='flex flex-col w-full my-20'>
-        <h2 className='text-3xl  ml-10'>{items4[0]}</h2>
+        {/* <h2 className='text-3xl  ml-10'>{items4[0]}</h2>
        {
         items4?.map((el,index) =>{
           return <Link key={index} className='w-96 ml-10 my-1 hover:text-grey hover:decoration-blue' href={`${el.path}`} >{el.name}</Link>
         })
-       } 
+       }  */}
     </div>
     );
 }
