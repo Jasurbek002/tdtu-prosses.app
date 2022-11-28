7b377ba74141bfd0a01e232099b25cb09b53218b
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2022-11-28 15:51:24 +0500

    remove

--- pages/jurnal/jurnal.js
+++ pages/jurnal/jurnal.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { items6 } from '../../components/data/items';
-const Jurnal = () => {
-    return (
-        <div className='flex flex-col w-full my-20'>
-        <h2 className='text-3xl  ml-10'>{items6[0]}</h2>
-       {
-        items6?.map((el,index) =>{
-          return <Link key={index} className='w-96 ml-10 my-1 hover:text-grey hover:decoration-blue' href={`${el.path}`} >{el.name}</Link>
-        })
-       } 
-    </div>
-    );
-}
+// import React from 'react';
+// import { items6 } from '../../components/data/items';
+// const Jurnal = () => {
+//     return (
+//         <div className='flex flex-col w-full my-20'>
+//         <h2 className='text-3xl  ml-10'>{items6[0]}</h2>
+//        {
+//         items6?.map((el,index) =>{
+//           return <Link key={index} className='w-96 ml-10 my-1 hover:text-grey hover:decoration-blue' href={`${el.path}`} >{el.name}</Link>
+//         })
+//        } 
+//     </div>
+//     );
+// }
 
-export default Jurnal;
+// export default Jurnal;
